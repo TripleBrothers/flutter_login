2200771a166b5b2c338938e0de3cd6b29ed66f70
junlee <jhlee9870@gmail.com>
2020-07-27 19:26:11 +0900

    add apple login and google login

--- lib/flutter_login.dart
+++ lib/flutter_login.dart
@@ -169,14 +169,14 @@ class __HeaderState extends State<_Header> {
         tag: widget.titleTag,
         largeFontSize: widget.loginTheme.beforeHeroFontSize,
         smallFontSize: widget.loginTheme.afterHeroFontSize,
-        style: GoogleFonts.pacifico(),
+        style: GoogleFonts.pacifico(textStyle: TextStyle(color: Colors.white, fontSize: 25)),
         viewState: ViewState.enlarged,
       );
     } else if (!DartHelper.isNullOrEmpty(widget.title)) {
       title = Text(
         widget.title,
         key: kTitleKey,
-        style: GoogleFonts.pacifico(),
+        style: GoogleFonts.pacifico(textStyle: TextStyle(color: Colors.white, fontSize: 25)),
       );
     } else {
       title = null;
